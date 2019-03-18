5cf2705cda739c90f45826d3c6810b6c159dd0dc
Marcos Minond <minond.marcos@gmail.com>
2019-03-18 15:22:39 -0600

    Removing addition method delegator

--- lib/protobuf/rpc/extensions/client.rb
+++ lib/protobuf/rpc/extensions/client.rb
@@ -2,14 +2,10 @@ module Protobuf
   module Opentracing
     module Extensions
       module Client
-        extend Forwardable
-
-        def_delegator :@connector, :send_request, :send_traced_request
-
         def send_request
           operation = "#{options[:service]}##{options[:method]}"
           ::OpenTracing.start_active_span(operation) do
-            send_traced_request
+            super
           end
         end
       end
