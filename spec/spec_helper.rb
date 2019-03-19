77450272affec387aafddb044ed3127ca5b95337
Marcos Minond <minond.marcos@gmail.com>
2019-03-19 11:28:45 -0600

    setting up nats connection

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,17 @@
 require "bundler/setup"
+
+require "protobuf"
+require "protobuf/nats"
 require "protobuf/opentracing"
+
+require "test_service"
+
+ENV["PROTOBUF_NATS_CONFIG_PATH"] = "./spec/support/protobuf_nats.yml"
+ENV["PB_NATS_SERVER_SLOW_START_DELAY"] = "0.1"
+
+::Protobuf.connector_type_class = ::Protobuf::Nats::Client
+::Protobuf::Nats.start_client_nats_connection
+::Protobuf::Logging.logger.level = ::Logger::UNKNOWN
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
