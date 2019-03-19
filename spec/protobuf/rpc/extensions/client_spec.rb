98eb00e88c02a8dcf91590025992121ff41aab4b
Marcos Minond <minond.marcos@gmail.com>
2019-03-19 12:07:12 -0600

    fixing linting error

--- spec/protobuf/rpc/extensions/client_spec.rb
+++ spec/protobuf/rpc/extensions/client_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe Protobuf::Opentracing::Extensions::Client do
   it "starts a new active span when making a request" do
     expect(::OpenTracing).to receive(:start_active_span).with("TestService#test_search")
-    client = ::Protobuf::Rpc::Client.new({ :service => TestService })
+    client = ::Protobuf::Rpc::Client.new(:service => TestService)
     client.test_search(::TestRequest.new)
   end
 end
