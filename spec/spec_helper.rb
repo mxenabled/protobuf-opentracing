0d3523c7152cd6777389f125066677517ecc2852
Marcos Minond <minond.marcos@gmail.com>
2019-03-19 11:41:30 -0600

    documenting config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,12 +6,19 @@ require "protobuf/opentracing"
 
 require "test_service"
 
+# Ensure we load the testing configuration
 ENV["PROTOBUF_NATS_CONFIG_PATH"] = "./spec/support/protobuf_nats.yml"
-ENV["PB_NATS_SERVER_SLOW_START_DELAY"] = "0.1"
 
+# For testing purposes we only need a single subscription per endpoint. Note
+# that if this is ever increased, the slow start delay should most likely be
+# decreased. See `PB_NATS_SERVER_SLOW_START_DELAY`.
+ENV["PB_NATS_SERVER_SUBSCRIPTIONS_PER_RPC_ENDPOINT"] = "1"
+
+# Use the NATS client by default and start the connection to the NATS server.
+# If this fails make sure that a NATS server is running and the configuration
+# found in `PROTOBUF_NATS_CONFIG_PATH` is correct.
 ::Protobuf.connector_type_class = ::Protobuf::Nats::Client
 ::Protobuf::Nats.start_client_nats_connection
-::Protobuf::Logging.logger.level = ::Logger::UNKNOWN
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
