0bcfd31e8f2e2e6871588fa6953ba6bac9298373
Marcos Minond <minond.marcos@gmail.com>
2019-04-10 10:57:50 -0600

    Starting a non-active span when an active span is detected. (#4)
    Using non-active spans when possible. Fixes an issue where a top-level span scope would be closed and raise and exception due to an active child span was still running.

--- lib/protobuf/rpc/extensions/client.rb
+++ lib/protobuf/rpc/extensions/client.rb
@@ -3,9 +3,21 @@ module Protobuf
     module Extensions
       module Client
         def send_request
-          operation = "#{options[:service]}##{options[:method]}"
-          ::OpenTracing.start_active_span(operation) do
-            super
+          span = start_span
+          results = super
+          span.finish
+          results
+        end
+
+        def operation_name
+          @operation_name ||= "#{options[:service]}##{options[:method]}"
+        end
+
+        def start_span
+          if ::OpenTracing.active_span
+            ::OpenTracing.start_span(operation_name)
+          else
+            ::OpenTracing.start_active_span(operation_name).span
           end
         end
       end
--- spec/protobuf/rpc/extensions/client_spec.rb
+++ spec/protobuf/rpc/extensions/client_spec.rb
@@ -1,7 +1,45 @@
 RSpec.describe Protobuf::Opentracing::Extensions::Client do
-  it "starts a new active span when making a request" do
-    expect(::OpenTracing).to receive(:start_active_span).with("TestService#test_search")
-    client = ::Protobuf::Rpc::Client.new(:service => TestService)
-    client.test_search(::TestRequest.new)
+  before do
+    ::OpenTracing.global_tracer = ::OpenTracingTestTracer.build
+    server.subscribe
+  end
+
+  after do
+    ::OpenTracing.global_tracer = ::OpenTracing::Tracer.new
+    server.unsubscribe
+    server.nats.close
+  end
+
+  let(:server) { ::Protobuf::Nats::Server.new(:threads => 1) }
+  let(:client) { ::Protobuf::Rpc::Client.new(:service => TestService, :method => "test_search") }
+
+  describe "#operation_name" do
+    it "uses the service name and request method for the operation name" do
+      expect(client.operation_name).to eq "TestService\#test_search"
+    end
+  end
+
+  describe "#send_request" do
+    it "starts a new active span when making a request" do
+      client.test_search(::TestRequest.new) do |c|
+        c.on_complete do |_|
+          expect(::OpenTracing.active_span.operation_name).to eq client.operation_name
+        end
+      end
+    end
+
+    it "does not start a new active span when one has already been created" do
+      ::OpenTracing.start_active_span("testing") do
+        client.test_search(::TestRequest.new) do |c|
+          c.on_complete do |_|
+            # Three spans: First one is the one we started in this test, second
+            # is the one started by the client, and the third is the one
+            # started by the server.
+            expect(::OpenTracing.global_tracer.spans.size).to be 3
+            expect(::OpenTracing.active_span.operation_name).to eq "testing"
+          end
+        end
+      end
+    end
   end
 end
